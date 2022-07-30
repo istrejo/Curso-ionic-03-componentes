92209a9c865bc5bb561b2ff1d2030c558c0fdac7
istrejo <istrejo2106@gmail.com>
2022-07-30 19:31:30 -0400

    Ion Loading

---
+++ src/app/pages/loading/loading.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.page.html',
+  styleUrls: ['./loading.page.scss'],
+})
+export class LoadingPage implements OnInit {
+  loading: HTMLIonLoadingElement;
+
+  constructor(private loadingCtrl: LoadingController) {}
+
+  ngOnInit() {}
+
+  async showLoading(message: string) {
+    this.loading = await this.loadingCtrl.create({
+      message,
+    });
+
+    await this.loading.present();
+    setTimeout(() => {
+      this.loading.dismiss();
+    }, 3000);
+  }
+}
