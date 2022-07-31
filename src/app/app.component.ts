7ff76f9aeeee6fc77d516c54d8061fedb5df7d8c
istrejo <istrejo2106@gmail.com>
2022-07-30 23:25:55 -0400

    ion Menu Toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Componente } from './core/models/interfaces';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  menuOpts: Observable<Componente[]>;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.getMenuOpts();
+  }
+
+  getMenuOpts() {
+    this.menuOpts = this.dataService.getMenuOpts();
+  }
 }
